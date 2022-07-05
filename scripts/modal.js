d269082dc2233fe4c65ebe96bf1f91bedfe49330
Eric Wilson <ewpiano@gmail.com>
2022-07-05 16:00:47 -0500

    updates modal show and hide functions

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,33 +1,33 @@
 function closeModal() {
-    const modal = document.querySelector('.modal');
-    modal.classList.add('hidden');
-  }
-  
-  function openModal() {
-    const modal = document.querySelector('.modal');
-    modal.classList.remove('hidden');
-  }
-  
-  document.querySelector('.modal-close').addEventListener('click', () => {
-    closeModal();
+  const modal = document.querySelector('.modal');
+  const modalContent = document.querySelectorAll('.details');
+  modal.classList.add('hidden');
+  modalContent.forEach(item => {
+    item.classList.add('hidden');
   });
-  
-  document.querySelectorAll('.modal-open-job').forEach(item => {
-    item.addEventListener('click', (e) => {
-      e.preventDefault();
-      openModal();
-    });
-  });
-  
-  document.querySelectorAll('.modal-open-education').forEach(item => {
-    item.addEventListener('click', (e) => {
-      e.preventDefault();
-      openModal();
-    });
-  });
+}
 
-  document.querySelector('.modal').addEventListener('click', (e) => {
-    if (e.target.tagName.toLowerCase() !== 'div') {
-      closeModal();
-    }
+function openModal(contentId) {
+  const modal = document.querySelector('.modal');
+  const modalContent = document.querySelector(`#${contentId}`);
+  modal.classList.remove('hidden');
+  modalContent.classList.remove('hidden');
+}
+
+document.querySelector('.modal-close').addEventListener('click', () => {
+  closeModal();
+});
+
+document.querySelectorAll('.modal-open').forEach(item => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    const modalId = item.dataset.modal;
+    openModal(modalId);
   });
+});
+
+document.querySelector('.modal').addEventListener('click', (e) => {
+  if (e.target.tagName.toLowerCase() !== 'div') {
+    closeModal();
+  }
+});
